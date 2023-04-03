8eb9048c4f8d09afe8098441a628347244e3f444
bennik06 <126697208+bennik06@users.noreply.github.com>
2023-04-03 20:11:04 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+namespace marinetower;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0.0";
+    public const string Name = "Full Marine Tower";
+
+    public const string Description = "Adds the Marine as a full tower!<br><br>" + "Thanks to everyone on the 1330 Studios Discord server who helped me make this mod.";
+ public const string SubPath = "marinetower";
+ public const string DllName = "Marine.dll";
+ public const string WorksOnVersion = "35.0";
+
+    public const string RepoOwner = "bennik";
+    public const string RepoName = "btd6_mods";
+}
